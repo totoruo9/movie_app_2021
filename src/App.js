b803bac19ca6f741839791ea01d17caf01a8b57e
Jin Hyuk <wlsgurrla10@naver.com>
2021-06-25 21:27:39 +0900

    yarn add prop-types
    create movie.js
    => propType을 설정하고 Movie를 함수를 내보내준다.
    
    App.js
    => Movie를 받아와 화면에 업데이트를 해준다.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import axios from 'axios';
+import Movie from './movies';
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movie: []
   };
-  componentDidMount(){
-    axios.get("https://yts-proxy.now.sh/list_movies.json")
+
+  getMoviees = async() => {
+    const {data: {data: {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({movies, isLoading: false});
   }
 
+  componentDidMount(){
+    this.getMoviees();
+  }
+
+
+
   render(){
-    const {isLoading} = this.state;
-  return <div>{isLoading ? 'Loading...' : 'Start App'}</div>
+    const {isLoading, movies} = this.state;
+  return <div>{isLoading ? 'Loading...' : movies.map(movie => {
+    return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
+  })}</div>
   }
 }
 
